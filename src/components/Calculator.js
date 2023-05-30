d8ac664c1d2a3740798b76c263a9559f35ca66a0
Romans S <romans.spilaks@gmail.com>
2023-05-30 06:20:26 +0300

    Change name of element screen to result. Change characters for division and multiplying. Switch places of a few button to follow template styles.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,15 +4,15 @@ function Calculator() {
   return (
     <div className="container">
       <div className="wrapper">
-        <div className="screen">0</div>
+        <div className="result">0</div>
         <div className="btn btn-grey">AC</div>
-        <div className="btn btn-grey">%</div>
         <div className="btn btn-grey">+/-</div>
-        <div className="btn btn-orange">/</div>
+        <div className="btn btn-grey">%</div>
+        <div className="btn btn-orange">÷</div>
         <div className="btn btn-grey">7</div>
         <div className="btn btn-grey">8</div>
         <div className="btn btn-grey">9</div>
-        <div className="btn btn-orange">x</div>
+        <div className="btn btn-orange">×</div>
         <div className="btn btn-grey">4</div>
         <div className="btn btn-grey">5</div>
         <div className="btn btn-grey">6</div>
