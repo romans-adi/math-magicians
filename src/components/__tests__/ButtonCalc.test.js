10dbe34e0c723554f9a21af8ae28ef8d4be9d747
J.Garcia <33934969+JuniorGarciaRodriguez@users.noreply.github.com>
2023-06-07 16:14:41 -0600

    Write unittest for the ButtonCalc component

---
+++ src/components/__tests__/ButtonCalc.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { JSDOM } from 'jsdom';
+import { render } from '@testing-library/react';
+import ButtonCalc from '../Calculator/ButtonCalc/ButtonCalc';
+
+const dom = new JSDOM('<!doctype html><html><body></body></html>', {
+  url: 'http://localhost/',
+});
+global.window = dom.window;
+global.document = dom.window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+describe('ButtonCalc', () => {
+  test('renders correctly', () => {
+    const onClick = jest.fn();
+    const color = 'my-color';
+    const className = 'my-class';
+    const { container } = render(
+      <ButtonCalc onClick={onClick} color={color} className={className}>
+        Bu Bu Button
+      </ButtonCalc>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
