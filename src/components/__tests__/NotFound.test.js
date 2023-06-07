e105025e0de591c4fd3bf4c7090111a149e843d9
J.Garcia <33934969+JuniorGarciaRodriguez@users.noreply.github.com>
2023-06-07 16:18:43 -0600

    Write unittest for the NotFound component
    If there is a 404 error

---
+++ src/components/__tests__/NotFound.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { JSDOM } from 'jsdom';
+import NotFound from '../NotFound/NotFound';
+
+const dom = new JSDOM('<!doctype html><html><body></body></html>', {
+  url: 'http://localhost/',
+});
+global.window = dom.window;
+global.document = dom.window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+describe('NotFound', () => {
+  test('renders correctly', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
